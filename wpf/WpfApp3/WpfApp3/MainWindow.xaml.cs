be6b01cfbb46dc61595ad9e18cf57d24e59edcdc
qwerty639 <said.abdurakhmanov.79@mail.ru>
2020-12-16 14:25:02 +0300

    gay

---
+++ wpf/WpfApp3/WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Expander_Expanded(object cender, RoutedEventArgs e)
+        {
+            ((Expander)sender) = new Button() { Width = 80, Height = 30, Content = "Пивет"};
+        }
+
+        private void Expander_Collapsed(object cender, RoutedEventArgs e)
+        {
+            MessageBox.Show(" Экспандер свернут ");
+        }
+    }
+}
